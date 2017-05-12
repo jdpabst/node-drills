41f6dd49cf150fb61d8550e60d7443d3f314ab47
Jessica Hollingshead <Jessica Hollingshead>
2017-05-12 11:17:13 -0600

    finished apps 1-7

--- app7-sessions/index.js
+++ app7-sessions/index.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var port = 3000;
 var app = express();
 
 app.use(bodyParser.json());
+app.use(session({
+  secret: 'alskjdflajeoifajfjaksldlkkdjfl;asjdf;alkjfsd;',
+  resave: true,
+  saveUninitialized: false
+}))
 
+app.get('/api/data', function(req, res, next){
+  res.status(200).send(req.session.data);
+})
+
+
+app.post('/api/data', function(req, res, next){
+  req.session.data = req.body;
+  res.status(200).send('it worked')
+})
 
 
 
